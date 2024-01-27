f2d0a1cc15656dcd0216e5e75769d738be26b188
iliyasrobin <iliyasahmedr@gmail.com>
2024-01-27 20:13:22 +0600

    connect

---
+++ first.py
@@ -0,0 +1,64 @@
+# print("Hello")
+# a = 543
+# b = 9
+# c = a + b
+# print(c)
+# if a == 5:
+#     print(c)
+# else:
+#     print(b)
+# #Comments
+# for i in range(1,4):
+#     print("*",end = " ")
+
+
+# #type casting
+# x = 34344
+# y = str(x)
+#
+# z = x,y
+# print(z)
+# print(type(z))
+# #boolean
+# p = 343444
+# print(x==p)
+#
+# #sets
+# thisisset = {'hello','hjht', '43','iliyas'}
+# set2 = {'a', 'b', 'c'}
+#
+# #add
+# thisisset.add("iliy")
+# print(thisisset)
+# thisisset.update(set2)
+# print(thisisset)
+#
+# #remove
+# thisisset.remove('43')
+# print(thisisset)
+# thisisset.discard('a')
+# print(thisisset)
+#
+# #Loop
+# for x in thisisset:
+#     print(x)
+#
+# #Join
+# set3 = thisisset.union(set2)
+# print('joined set is:',set3)
+#
+
+# #graph:
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# xpoints = np.array([1, 2,3,4,5,6,7,8,9])
+# ypoints = np.array([3, 11, 33,445,66,1, 3,44, 7])
+#
+# plt.plot(xpoints, ypoints)
+# plt.show()
+#
+
+# #for loop
+# for i in range(1,10):
+#     print(i)
---
+++ print.py
@@ -0,0 +1,21 @@
+print("hello world")
+print('hello world')
+a = "my name is: "
+b = "iliyas"
+c = 10
+
+print(a+b)
+print("my name is: "+b)
+print("my name is: ", b)
+print(a+b+str(c)) #type casting : int to string
+print(a+b,c)
+print(type(c))
+
+d = ("10")
+e = int(d) #string to int
+print(e)
+print(type(e)) #type() is for viewing type of variables
+
+
+
+
